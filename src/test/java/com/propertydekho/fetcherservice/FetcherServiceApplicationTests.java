5a59e1428c78eafed5371af9beae46ab71bff1fd
Divsjains6021 <divsjains6021@gmail.com>
2020-05-10 00:22:12 +0530

    Property Fetcher Service

---
+++ src/test/java/com/propertydekho/fetcherservice/FetcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertydekho.fetcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetcherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
