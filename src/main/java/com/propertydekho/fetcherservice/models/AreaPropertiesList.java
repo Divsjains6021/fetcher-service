5de2d7bd07935543fa493e773f2fe3ba3da411fa
sameer.jain <sameer.jain@flipkart.com>
2020-05-24 21:52:45 +0530

    Added code for including more filters and also for multiple area support

--- src/main/java/com/propertydekho/fetcherservice/models/AreaPropertiesList.java
+++ src/main/java/com/propertydekho/fetcherservice/models/AreaPropertiesList.java
@@ -4,19 +4,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @Builder
 public class AreaPropertiesList
 {
     @JsonProperty("index_props")
-    private PropMetaDataList indexedProperties;
+    private Map<String, PropMetaDataList> indexedProperties;
     @JsonProperty("non_index_props")
     private PropMetaDataList nonIndexedProperties;
 
     public AreaPropertiesList() {
     }
 
-    public AreaPropertiesList(PropMetaDataList indexedProperties, PropMetaDataList nonIndexedProperties) {
+    public AreaPropertiesList(Map<String, PropMetaDataList> indexedProperties, PropMetaDataList nonIndexedProperties) {
         this.indexedProperties = indexedProperties;
         this.nonIndexedProperties = nonIndexedProperties;
     }
--- src/main/java/com/propertydekho/fetcherservice/models/PropFilterableSortableData.java
+++ src/main/java/com/propertydekho/fetcherservice/models/PropFilterableSortableData.java
@@ -13,8 +13,6 @@ public class PropFilterableSortableData {
     private String propName;
     @JsonProperty("prop_price")
     private double propPrice;
-    @JsonProperty("budget_max")
-    private double budgetMax;
     @JsonProperty("bedroom")
     private String bedroom;
     @JsonProperty("sale_type")
@@ -28,11 +26,11 @@ public class PropFilterableSortableData {
 
     }
 
-    public PropFilterableSortableData(String propID, String propName, double propPrice, double budgetMax, String bedroom, String saleType, String constructionStatus, String area) {
+    public PropFilterableSortableData(String propID, String propName, double propPrice, String bedroom,
+                                      String saleType, String constructionStatus, String area) {
         this.propID = propID;
         this.propName = propName;
         this.propPrice = propPrice;
-        this.budgetMax = budgetMax;
         this.bedroom = bedroom;
         this.saleType = saleType;
         this.constructionStatus = constructionStatus;
